0b6191f3c714a852b729f77ecc9596bbe28f1755
Om Tayade <omtayade160901@gmail.com>
2020-12-30 00:38:24 +0530

    Added Headers and selected logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shopPage/shop.component'
+import Header from './components/headers/headers.component'
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
 
@@ -9,6 +10,7 @@ import {Route, Switch} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <Header/>
     <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route exact path='/shop' component={ShopPage} />
