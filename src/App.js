e68bf163147d8ffd54a0c1f8f21a9efa81fbf88d
Om Tayade <omtayade160901@gmail.com>
2020-12-29 10:45:02 +0530

    Shop page ,Collection Preview , Collection-Item are developed partly but they are still non functional

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shopPage/shop.component'
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
 
-const HatsPage = () =>(
- <div>
-   <h1>HATS PAGE</h1>
- </div>
-);
+
 
 function App() {
   return (
     <div className="App">
-    
+    <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route  path='/shop/hats' component={HatsPage} />
+      <Route exact path='/shop' component={ShopPage} />
+    </Switch>
+      
 
 
    
