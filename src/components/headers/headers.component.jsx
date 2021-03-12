ca7341f9e57284b3ae95dffcb3e7fe17c1bfdee6
Om Tayade <omtayade160901@gmail.com>
2021-03-12 16:14:34 +0530

    Added cartIcon and cartComponent and implemented cartReducer thus redux for conditional rendering of cart dropdown

--- src/components/headers/headers.component.jsx
+++ src/components/headers/headers.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {connect} from 'react-redux'
 import { auth } from '../../firebase/firebase.utils';
-
+import CartDropdown from '../Cart-dropdown/cart-dropdown.component'
 import { ReactComponent as Logo } from '../../assets/cart.svg';
-
+import CartIcon from '../Cart-dropdown/cartIcon.component'
 import './headers.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser , hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -28,13 +28,18 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon/>
     </div>
+    {hidden?null:<CartDropdown/>}
+    
   </div>
 );
 
 
-const mapStateToProps = state =>({
-  currentUser: state.user.currentUser  //Equal as=> currentUser:rootReducer.userReducer.currentUser
+const mapStateToProps = ({user:{currentUser} , cart:{hidden}}) =>({
+  // currentUser: state.user.currentUser  //Equal as=> currentUser:rootReducer.userReducer.currentUser
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps) (Header);
