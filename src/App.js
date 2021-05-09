b1b560e9339cb72b77c6975c746f1c1ea299b057
Mayur Jain <maxcharge788769@gmail.com>
2021-05-09 17:36:16 +0530

    Revert "Added toasts"
    This reverts commit e257b07f54082a225bb55edeacdbd60c7698b8f3.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 // import Header from './components/headers/headers.component'
 import { auth, createUserProfile } from "./firebase/firebase.utils";
 import "./App.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
@@ -11,11 +9,12 @@ import { selectCurrentUser } from "./redux/user/user.selectors";
 import { createStructuredSelector } from "reselect";
 import AboutUs from "./pages/About us/about-us.component";
 
+import Body from "./pages/events/Body";
+
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-import Body from "./pages/events/Body";
 import SignIn from "./pages/sign-in/sign-in.component";
 import SignUp from "./pages/Sign-up/sign-up.component";
 import ForgotPassword from "./components/forgot-password/forgot-password.component";
@@ -49,17 +48,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Navbar />
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
+
         <Switch>
           <Route
             exact
--- src/pages/sign-in/sign-in.component.jsx
+++ src/pages/sign-in/sign-in.component.jsx
@@ -1,92 +1,82 @@
-import React from "react";
-import { toast } from "react-toastify";
-import FormInput from "../../components/form-input/form-item.component";
-import CustomButton from "../../components/custom-button/custom-button.component";
-import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
-import "./sign-in.styles.scss";
-import { Link } from "react-router-dom";
+import React from 'react'
+import FormInput from '../../components/form-input/form-item.component'
+import CustomButton from '../../components/custom-button/custom-button.component'
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils'
+import './sign-in.styles.scss'
+import { Link } from 'react-router-dom'
 
-class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      email: "",
-      password: "",
-    };
-  }
-  handleSubmit = async (event) => {
-    event.preventDefault();
-    const { email, password } = this.state;
 
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-
-      this.setState({ email: "", password: "" });
-    } catch (error) {
-      console.log(error);
-      toast.error(error.message, {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
+class SignIn extends React.Component{
+    constructor(props){
+        super(props);
+    
+        this.state={
+            email:'',
+            password:''
+        }
     }
-  };
+    handleSubmit = async event=>{
+        event.preventDefault();
+        const {email , password} = this.state;
 
-  handleChange = (event) => {
-    const { name, value } = event.target;
+        try{
+            await auth.signInWithEmailAndPassword(email , password);
 
-    this.setState({ [name]: value });
-  };
+            this.setState({email:'' , password:''})
+        }
+        catch(error){
+            console.log(error);
+            alert(error.message)
+        }        
 
-  render() {
-    return (
-      <div className="box">
-        <div className="sign-in">
-          <h2 style={{ color: "black" }}>I already have an account</h2>
-          <span style={{ color: "black" }}>
-            Sign in with your email and password
-          </span>
+        
+    }
 
-          <form onSubmit={this.handleSubmit}>
-            <FormInput
-              type="email"
-              name="email"
-              label="Email"
-              value={this.state.email}
-              handleChange={this.handleChange}
-              required
-            />
+    handleChange = event =>{
+        const {name ,value} = event.target;
 
-            <FormInput
-              type="password"
-              name="password"
-              label="Password"
-              minLength="5"
-              value={this.state.password}
-              handleChange={this.handleChange}
-              required
-            />
-            <div className="buttons">
-              <CustomButton type="submit">Sign In</CustomButton>
-              <Link to="/signin/forgot-password">
-                <CustomButton isGoogleSignIn>Forgot Password</CustomButton>
-              </Link>
+        this.setState({ [name]:value})
+    }
+
+     
+
+    render(){
+        return(
+            <div className="box">
+                <div className="sign-in">
+                    <h2 style={{color:"black"}}>I already have an account</h2>
+                    <span style={{color:"black"}}>Sign in with your email and password</span>
+
+                    <form onSubmit={this.handleSubmit} >
+                        <FormInput type="email" name="email" label="Email" value={this.state.email} handleChange={this.handleChange} required/>
+                        
+                        <FormInput type="password" name="password" label="Password" value={this.state.password} handleChange={this.handleChange} required/>
+                    <div className='buttons'>
+                        <CustomButton type="submit" >Sign In</CustomButton>
+                        <Link to='/signin/forgot-password'>
+                            <CustomButton isGoogleSignIn >Forgot Password</CustomButton>
+                        </Link>
+                    </div>
+                    
+
+                    </form>
+                
+                </div>
+                <div className="switchPage">
+                    <h3 style={{color:"black" }}>Don't have an account?</h3>
+                    <Link to='/signup'>
+                    <h4 style={{color:"red" }}>  Register</h4>
+                    </Link>
+                    
+                </div>
+            
+
             </div>
-          </form>
-        </div>
-        <div className="switchPage">
-          <h3 style={{ color: "black" }}>Don't have an account?</h3>
-          <Link to="/signup">
-            <h4 style={{ color: "red" }}> Register</h4>
-          </Link>
-        </div>
-      </div>
-    );
-  }
+        )
+
+    }
+
+
 }
-export default SignIn;
+export default SignIn
