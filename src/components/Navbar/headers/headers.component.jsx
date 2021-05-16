cda7bfe4ea69db4892b11e40a44a0b7efca4e0d7
Om Tayade <omtayade160901@gmail.com>
2021-05-16 09:14:52 +0530

    Header onClick Hamburger bug resolved

--- src/components/Navbar/headers/headers.component.jsx
+++ src/components/Navbar/headers/headers.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
@@ -11,7 +12,31 @@ import styled from "styled-components";
 
 import "./headers.component.css";
 
-
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  min-width: 70%;
+  margin: 0;
+  li {
+    padding: 18px 10px;
+  }
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: #e85d04;
+    position: fixed;
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+    top: 0;
+    right: 0;
+    height: 100%;
+    padding-top: 3.5rem;
+    z-index: 100;
+    transition: transform 0.3s ease-in-out;
+    li .option {
+      color: white;
+    }
+  }
+`;
 
 const handleClick = async () => {
   await auth.signOut();
@@ -19,8 +44,8 @@ const handleClick = async () => {
 };
 
 const RightHeader = ({ open, currentUser }) => (
-
-  <ul open={open}>
+  <Ul open={open}>
+    
     <li>
       <NavLink
         to="/"
@@ -31,70 +56,48 @@ const RightHeader = ({ open, currentUser }) => (
         Home
       </NavLink>
     </li>
+
     <li>
       <NavLink
         to="/about-us"
         className="option"
         activeClassName="active__option"
       >
-        About
+        About Us
       </NavLink>
     </li>
-    <li>
-      <NavLink to="/theme" className="option" activeClassName="active__option">
-        Theme
-      </NavLink>
-    </li>
-    <li>
-      <NavLink
-        to="/speakers"
-        className="option"
-        activeClassName="active__option"
-      >
-        Speakers
-      </NavLink>
-    </li>
-    <li>
-      <NavLink to="/events" className="option" activeClassName="active__option">
-        Events
-      </NavLink>
-    </li>
-    <li>
-      <NavLink to="/team" className="option" activeClassName="active__option">
-        Team
-      </NavLink>
-    </li>
-    <li>
-      <NavLink
-        to="/contact-us"
-        className="option"
-        activeClassName="active__option"
-      >
-        Contact Us
-      </NavLink>
-    </li>
-    <li>
-      {currentUser ? (
-        <NavLink
-          className="option"
-          to="/signin"
-          onClick={handleClick}
-          activeClassName="active__option"
-        >
-          SIGN OUT
-        </NavLink>
-      ) : (
-        <NavLink
-          className="option"
-          to="/signup"
-          activeClassName="active__option"
-        >
-          Login/Register
-        </NavLink>
-      )}
-    </li>
-  </ul>
 
+      <li>
+        <NavLink  to='/events' className='option' activeClassName="active__option">
+          Events
+        </NavLink>
+      </li>
+      <li>
+        <NavLink  to='/developers' className='option' activeClassName="active__option">
+          Developers
+        </NavLink>
+      </li>
+      <li>
+        <NavLink  to='/contact-us' className='option' activeClassName="active__option" >
+          Contact Us
+        </NavLink>
+      </li>
+      <li>
+        {currentUser ? (
+          <NavLink className='option' to='/signin' activeClassName="active__option" onClick={handleClick} >
+            SIGN OUT
+          </NavLink>
+        ) : (
+          <NavLink className='option' activeClassName="active__option" to='/signup' >
+            Login/Register
+          </NavLink>
+        )}
+
+      </li>
+      
+      
+     
+  </Ul>
 );
 
 const mapStateToProps = createStructuredSelector({
