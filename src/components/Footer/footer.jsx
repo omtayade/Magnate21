75b04f756c1c7e3ad276cdcaaad742607e65a9cb
Amit Purohit <amitpurohit4747@gmail.com>
2021-05-19 17:48:10 +0530

    temp

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,85 +1,78 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import './footer.css';
 
 class Footer extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.handleScrollToStats=this.handleScrollToStats.bind(this);
+        this.handleScrollToStats = this.handleScrollToStats.bind(this);
     }
-    
+
     handleScrollToStats = () => {
         window.scrollTo({
             top: 0
         })
-   }
+    }
 
-   render(){
-    return(
-        <div className="footer">
-            <div className="container">
-                 
-                    <div className="button-top">
-                        <Button outline onClick={this.handleScrollToStats} style={{color:"#f09103"}}>Back To Top</Button>
-                    </div>
+    render() {
+        return (
+            <div className="footer">
+
+                <div className="button-top">
+                    <Button outline onClick={this.handleScrollToStats} style={{ color: "#f09103" }}>Back To Top</Button>
+                </div>
+
                 
 
-                <div className="row" >
-                    <div className="col-12 align-self-center">
-                        <div className="text-center">
-                        <a href="www.facebook.com">
-                            <i className="fa fa-facebook-square" style={{fontSize:"2em"}}></i>
-                        </a>
-                        <a href="www.instagram.com">
-                            <i className="fa fa-instagram" style={{fontSize:"2em"}}></i>
-                        </a>
-                        <a href="www.linkedin.com">
-                            <i className="fa fa-linkedin" style={{fontSize:"2em"}}></i>
-                        </a>
-                        <a href="www.youtube.com">
-                            <i className="fa fa-youtube" style={{fontSize:"2em"}}></i>
-                        </a>
-                        </div> 
+                <div className="footer-content">
+                    <div className="quick-links">
+                        <h5 style={{ color: "grey" }}>Quick Links</h5>
+                        <a href="/events">events</a>
+                        <a href="/sponsers">sponsers</a>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-4 text-center quicklinks">
-                        <h5 style={{color:"grey"}}>Quick Links</h5>
-                        <a href="/events">events</a><br/>
-                        <a href="/sponsers">sponsers</a><br/>
-                    </div>
-                    <div className="logos col-4 text-center">
+                    <div className="logos">
                         <img src="images/magnate21.png" alt="MAGNATE21" />
-                        <p style={{color:"grey",paddingTop:"1em"}}>Organised by</p>
+                        <p style={{ color: "grey", paddingTop: "1em",marginLeft:"15%" }}>Organised by</p>
                         <img src="images/edc.png" alt="EDCPICT" />
                     </div>
-                    <div className="col-4">
-                        <div className="text-center">
-                            <h5 style={{color:"white"}}>Contact Us</h5>
-                            <address>
-                              <i className="fa fa-phone fa-lg" style={{color:"white"}}>  fdsl flkjs : </i><a style={{textDecoration:"none"}} href="callto:+852 1234 5678"><h5>+852 1234 5678</h5></a><br />
-                              <i className="fa fa-phone fa-lg" style={{color:"white"}}>  fdsl flkjs : </i><a style={{textDecoration:"none"}} href="callto:+852 1234 5678"><h5>+852 1234 5678</h5></a><br />
-                            </address>
-                        </div>    
-                    </div>
-                </div>  
-    
-                    
-                
-                <div className="row justify-content-center">             
-                    <div className="col-auto">
-                        <p className="text-center" style={{color:"#f09103"}}>©PICT EDC, MAGNATE21</p>
-                        <p className="text-center" style={{color:"#f09103"}}>IDEATE - INNOVATE - INCUBATE</p>
+                    <div className="contact">
+                        <h5 style={{ color: "grey" }}>Contact Us</h5>
+                        <address>
+                            <i className="fa fa-phone fa-lg" >  fdsl flkjs : </i><br/><a  href="callto:+852 1234 5678">+852 1234 5678</a><br />
+                            <i className="fa fa-phone fa-lg" >  fdsl flkjs : </i><br/><a  href="callto:+852 1234 5678">+852 1234 5678</a><br />
+                        </address>
                     </div>
                 </div>
-    
-                
+
+                <div className="end">
+                    <div className="copyright">
+                        <p style={{ color: "#f09103" }}>©PICT EDC, MAGNATE21</p>
+                    </div>
+                    <div className="message">
+                        <p style={{ color: "#f09103" }}>IDEATE - INNOVATE - INCUBATE</p>
+                    </div>
+                </div>
+
+                <div className="social-icons" >
+                    <a href="www.facebook.com">
+                        <i className="fab fa-facebook" style={{ fontSize: "2em" }}></i>
+                    </a>
+                    <a href="www.instagram.com">
+                        <i className="fab fa-instagram" style={{ fontSize: "2em" }}></i>
+                    </a>
+                    <a href="www.linkedin.com">
+                        <i className="fab fa-linkedin-in" style={{ fontSize: "2em" }}></i>
+                    </a>
+                    <a href="www.youtube.com">
+                        <i className="fab fa-youtube" style={{ fontSize: "2em" }}></i>
+                    </a>
+                </div>
+
             </div>
-        </div>
         );
-   }
-    
+    }
+
 }
 
 export default Footer;
--- src/pages/About us/about-us.component.jsx
+++ src/pages/About us/about-us.component.jsx
@@ -1,44 +1,48 @@
 import React from 'react';
 import './aboutus.css';
 import img1 from '../../assets/img1.gif';
+import Footer from '../../components/Footer/footer';
 
 const AboutUs = () =>(
-    <div className="aboutus">
-        <div className="magnate-theme">
-            <h1>THEME</h1>
-            <p>EVOLUTION of ENTREPRENEURSHIP</p>
-        </div>
-        <div className="about-magnate">
-            <h1>ABOUT MAGNATE'21</h1>
-            <div className="about-magnate-container">
-                <div className="about-magnate-content">
-                    The event is especially well recognized for the variety of panel discussions, competitive events,
-                    entrepreneurial opportunities, and the prominent and thought evoking speaker sessions that we
-                    host. MAGNATE’21 has a plethora of opportunities for all the entrepreneurship, start-ups,
-                    businesses, finance, marketing, and trading enthusiasts. The event will provide all the people from
-                    diverse backgrounds an opportunity to come together, compete, and share their entrepreneurial
-                    aspirations and experiences and get the right direction for their endeavors.
+    <div>
+        <div className="aboutus">
+            <div className="magnate-theme">
+                <h1>THEME</h1>
+                <p>EVOLUTION of ENTREPRENEURSHIP</p>
+            </div>
+            <div className="about-magnate">
+                <h1>ABOUT MAGNATE'21</h1>
+                <div className="about-magnate-container">
+                    <div className="about-magnate-content">
+                        The event is especially well recognized for the variety of panel discussions, competitive events,
+                        entrepreneurial opportunities, and the prominent and thought evoking speaker sessions that we
+                        host. MAGNATE’21 has a plethora of opportunities for all the entrepreneurship, start-ups,
+                        businesses, finance, marketing, and trading enthusiasts. The event will provide all the people from
+                        diverse backgrounds an opportunity to come together, compete, and share their entrepreneurial
+                        aspirations and experiences and get the right direction for their endeavors.
+                    </div>
+                    <div className="about-magnate-svg">
+                        <img src={img1} style={{width:'100%',height:"100%",borderRadius:"10px"}}></img>
+                    </div>
                 </div>
-                <div className="about-magnate-svg">
-                    <img src={img1} style={{width:'100%',height:"100%",borderRadius:"10px"}}></img>
+            </div>
+            <div className="about-edc">
+                <h1>ABOUT EDC</h1>
+                <div className="about-edc-container">
+                    <div className="about-edc-svg">
+                        <img src={img1} style={{width:'100%',height:"100%",borderRadius:"10px"}}></img>
+                    </div>
+                    <div className="about-edc-content">
+                        Entrepreneurship Development Cell, PICT firmly believes that India needs the drive and innovation of the young entrepreneurs.
+                        EDC therefore aims at manifesting the latent Entrepreneurial spirit of the youth.
+                        EDC was established by the college students, alumni, industry, and faculty,
+                        towards the cause of producing quality leaders who contribute to the growth and development of mankind.
+                        EDC was inaugurated on 4th march , 2009.
+                    </div>
                 </div>
             </div>
         </div>
-        <div className="about-edc">
-            <h1>ABOUT EDC</h1>
-            <div className="about-edc-container">
-                <div className="about-edc-svg">
-                    <img src={img1} style={{width:'100%',height:"100%",borderRadius:"10px"}}></img>
-                </div>
-                <div className="about-edc-content">
-                    Entrepreneurship Development Cell, PICT firmly believes that India needs the drive and innovation of the young entrepreneurs.
-                    EDC therefore aims at manifesting the latent Entrepreneurial spirit of the youth. 
-                    EDC was established by the college students, alumni, industry, and faculty, 
-                    towards the cause of producing quality leaders who contribute to the growth and development of mankind. 
-                    EDC was inaugurated on 4th march , 2009.
-                </div>
-            </div>
-        </div>
+        <Footer />
     </div>
 
 
